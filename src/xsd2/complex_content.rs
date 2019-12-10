19221196a1b701ec22ba51b35e70be01a4aa5596
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2019-12-10 17:37:54 +0700

    Add complex content

---
+++ src/xsd2/complex_content.rs
@@ -0,0 +1,14 @@
+use crate::xsd2::sequence::Sequence;
+use crate::xsd2::utils::find_child;
+use crate::xsd2::extension::Extension;
+
+pub struct ComplexContent<'a, 'input> {
+    pub node: roxmltree::Node<'a, 'input>,
+}
+
+impl<'a, 'input: 'a> ComplexContent<'a, 'input> {
+
+    pub fn extension(&self) -> Option<Extension<'a, 'input>> {
+        find_child(&self.node, "extension").map(|node| Extension{node})
+    }
+}
