5928ebb9922d69091c77414f6de7fbe581ffb5c3
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2019-12-28 00:59:06 +0700

    wip

--- src/generator/struct_field.rs
+++ src/generator/struct_field.rs
@@ -1,8 +1,9 @@
 use core::fmt;
 use crate::xsd2::complex_type::Attribute;
-use crate::generator::utils::{get_field_name, match_type, get_field_comment};
+use crate::generator::utils::{get_field_name, match_type, get_field_comment, get_type_name};
 use crate::generator::complex_type::{attribute_type, yaserde_for_attribute, element_type, yaserde_for_element};
 use crate::xsd2::sequence::{Element, Sequence};
+use crate::xsd2::extension::Extension;
 
 pub struct StructField {
     pub name: String,
@@ -62,15 +63,36 @@ pub  fn field_from_element(elem: &Element, target_namespace: Option<&str>) -> St
 }
 
 pub fn get_fields_from_sequence(s: &Sequence, target_namespace: Option<&str>) -> Vec<StructField> {
-        let mut fields = s.elements().
-            iter().
-            map(|el| field_from_element(el, target_namespace)).
-            collect::<Vec<StructField>>();
+    let mut fields = s.elements().
+        iter().
+        map(|el| field_from_element(el, target_namespace)).
+        collect::<Vec<StructField>>();
 
-        let any = s.any_element();
-        match any {
-            Some(_) => fields.push(any_element_field()),
-            None => ()
-        }
-        fields
+    let any = s.any_element();
+    match any {
+        Some(_) => fields.push(any_element_field()),
+        None => ()
     }
+    fields
+}
+
+pub fn get_fields_from_extension(ext: &Extension, target_namespace: Option<&str>) -> Vec<StructField> {
+    let mut fields = match ext.sequence() {
+        Some(s) => get_fields_from_sequence(&s, target_namespace),
+        None => vec!()
+    };
+
+    let ty = ext.base();
+    fields.push(StructField{
+        name: "base".to_string(),
+        typename: get_type_name(ty, target_namespace),
+        macros: yaserde_for_element("base"), //TODO: yaserde for base element
+        comment: String::new()
+    });
+
+    if ext.has_any_attribute() {
+        fields.push(any_attribute_field());
+    };
+
+    fields
+}
---
+++ src/xsd2/choice.rs
@@ -0,0 +1,21 @@
+use crate::xsd2::utils::{find_child};
+use crate::xsd2::utils::{MaxOccurs, MinOccurs, get_documentation, get_node_name, get_node_type, max_occurs, min_occurs};
+use crate::xsd2::sequence::Element;
+
+pub struct Choice<'a, 'input> {
+    pub node: roxmltree::Node<'a, 'input>,
+}
+
+impl<'a, 'input: 'a> Choice<'a, 'input> {
+
+    pub fn max_occurs(&self) -> MaxOccurs { max_occurs(&self.node) }
+    pub fn min_occurs(&self) -> MinOccurs { min_occurs(&self.node) }
+
+    pub fn elements(&self) -> Vec<Element> {
+        self.node.
+            children().
+            filter(|node| node.is_element() && node.tag_name().name() == "element").
+            map(|node| Element{node}).
+            collect::<Vec<Element>>()
+    }
+}
