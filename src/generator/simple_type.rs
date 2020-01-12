05a013341d7370ddebed02a08a53b3c48bd30ba4
SoVictor <sovictor_tomsk@hotmail.com>
2020-01-12 20:39:52 +0700

    Renamed functions

--- src/generator/simple_type.rs
+++ src/generator/simple_type.rs
@@ -3,7 +3,6 @@ use std::borrow::Cow;
 use crate::generator::utils::{get_field_comment, match_type};
 use crate::generator::enumeration::EnumCase;
 
-
 pub fn get_enum_facets<'a, 'input>(restriction: &Restriction<'a, 'input>) -> Vec<Facet<'a, 'input>> {
     restriction.facets().
     into_iter().
@@ -13,18 +12,18 @@ pub fn get_enum_facets<'a, 'input>(restriction: &Restriction<'a, 'input>) -> Vec
     }).collect()
 }
 
-pub fn enum_case2(facet : &Facet, target_namespace: Option<&str>) -> EnumCase {
+pub fn get_enum_cases(facets: &Vec<Facet>, target_namespace: Option<&str>) -> Vec<EnumCase> {
+    facets.
+        iter().
+        map(|f| get_enum_case(f, target_namespace)).
+        collect()
+}
+
+pub fn get_enum_case(facet : &Facet, target_namespace: Option<&str>) -> EnumCase {
     EnumCase{
         comment: get_field_comment(facet.documentation()),
         name: match_type(facet.value(), target_namespace).to_string(),
         value: facet.value().to_string(),
         typename: None
     }
-}
-
-pub fn enum_cases2(facets: &Vec<Facet>, target_namespace: Option<&str>) -> Vec<EnumCase> {
-    facets.
-        iter().
-        map(|f| enum_case2(f, target_namespace)).
-        collect()
 }
