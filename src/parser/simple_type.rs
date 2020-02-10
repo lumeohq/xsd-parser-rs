c185de0cb6d0dae405a7697da51615ff7f002325
leonid.krutovsky <leonid.krutovsky@invitae.com>
2020-02-10 13:17:19 +0700

    ch237: fix uses and tabulation in tests

--- src/parser/simple_type.rs
+++ src/parser/simple_type.rs
@@ -143,27 +143,28 @@ fn get_enum_case(node: &Node, target_ns: Option<&roxmltree::Namespace>) -> EnumC
 #[test]
 fn test_parse_simple_type_with_list() {
     let doc = roxmltree::Document::parse(
-        r#"<xs:schema xmlns:tt="http://www.onvif.org/ver10/schema" xmlns:xs="http://www.w3.org/2001/XMLSchema" targetNamespace="http://www.onvif.org/ver10/schema">
-                    <xs:simpleType name="SomeType">
-                    	<xs:annotation>
-							<xs:documentation>
-							    Some text
-							</xs:documentation>
-						</xs:annotation>
-                        <xs:list>
-                            <xs:simpleType>
-                                <xs:list itemType="xs:SSD"/>
-                            </xs:simpleType>
-                        </xs:list>
-                    </xs:simpleType>
-                </xs:schema>"#
+r#"
+<xs:schema xmlns:tt="http://www.onvif.org/ver10/schema" xmlns:xs="http://www.w3.org/2001/XMLSchema" targetNamespace="http://www.onvif.org/ver10/schema">
+    <xs:simpleType name="SomeType">
+        <xs:annotation>
+            <xs:documentation>
+                Some text
+            </xs:documentation>
+        </xs:annotation>
+        <xs:list>
+            <xs:simpleType>
+                <xs:list itemType="xs:SSD"/>
+            </xs:simpleType>
+        </xs:list>
+    </xs:simpleType>
+</xs:schema>
+"#
     ).unwrap();
 
     let schema = doc.root_element();
     let simple_type = find_child(&schema, "simpleType").unwrap();
     let target_ns = schema.namespaces()[0].clone();
 
-    let result = parse_simple_type(&simple_type, &schema, Some(&target_ns));
     match parse_simple_type(&simple_type, &schema, Some(&target_ns)) {
         RsEntity::TupleStruct(ts) => {
             assert_eq!(ts.name, "SomeType");
@@ -173,36 +174,35 @@ fn test_parse_simple_type_with_list() {
         }
         _ => unreachable!("Test failed!"),
     }
-
-    println!("{}", result);
 }
 
 #[test]
 fn test_parse_simple_type_with_restriction() {
     let doc = roxmltree::Document::parse(
-        r#"<xs:schema xmlns:tt="http://www.onvif.org/ver10/schema" xmlns:xs="http://www.w3.org/2001/XMLSchema" targetNamespace="http://www.onvif.org/ver10/schema">
-                    <xs:simpleType name="SomeType">
-                    	<xs:annotation>
-							<xs:documentation>
-							    Some text
-							</xs:documentation>
-						</xs:annotation>
-                        <xs:list>
-                            <xs:simpleType>
-                                <xs:restriction base="xs:string">
-                                    <xs:enumeration value="One"/>
-                                    <xs:enumeration value="Two"/>
-                                </xs:restriction>
-                            </xs:simpleType>
-                        </xs:list>
-                    </xs:simpleType>
-                </xs:schema>"#
+r#"
+<xs:schema xmlns:tt="http://www.onvif.org/ver10/schema" xmlns:xs="http://www.w3.org/2001/XMLSchema" targetNamespace="http://www.onvif.org/ver10/schema">
+    <xs:simpleType name="SomeType">
+        <xs:annotation>
+            <xs:documentation>
+                Some text
+            </xs:documentation>
+        </xs:annotation>
+        <xs:list>
+            <xs:simpleType>
+                <xs:restriction base="xs:string">
+                    <xs:enumeration value="One"/>
+                    <xs:enumeration value="Two"/>
+                </xs:restriction>
+            </xs:simpleType>
+        </xs:list>
+    </xs:simpleType>
+</xs:schema>
+                "#
     ).unwrap();
 
     let schema = doc.root_element();
     let simple_type = find_child(&schema, "simpleType").unwrap();
     let target_ns = schema.namespaces()[0].clone();
-    let result = parse_simple_type(&simple_type, &schema, Some(&target_ns));
 
     match parse_simple_type(&simple_type, &schema, Some(&target_ns)) {
         RsEntity::TupleStruct(ts) => {
@@ -220,6 +220,4 @@ fn test_parse_simple_type_with_restriction() {
         }
         _ => unreachable!("Test failed!"),
     }
-
-    println!("{}", result);
 }
