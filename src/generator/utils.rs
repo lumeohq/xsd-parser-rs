bf99309bba7f4d9cdcc354e4b5febe6af432f94b
leonid.krutovsky <leonid.krutovsky@invitae.com>
2020-02-17 18:52:39 +0700

    ch372: Add tuple struct generator

---
+++ src/generator/utils.rs
@@ -0,0 +1,27 @@
+
+pub fn split_comment_line(s: &str, max_len: usize, indent: usize) -> String {
+    let indent_str = " ".repeat(indent);
+
+    let mut splitted = format!("{}//", indent_str);
+    let mut current_line_length = indent + 2;
+    for word in s.split_whitespace() {
+        let len = word.len();
+        if current_line_length + len < max_len {
+            splitted = format!("{} {}", splitted, word);
+            current_line_length += 1 + len;
+        } else {
+            splitted = format!("{}\n{}// {}", splitted, indent_str, word);
+            current_line_length = indent + 3 + len;
+        }
+    }
+    format!("{}\n", splitted)
+}
+
+pub fn get_formatted_comment(doc: Option<&str>) -> String {
+    doc.unwrap_or("")
+        .lines()
+        .map(|s| s.trim())
+        .filter(|s| s.len() > 1)
+        .map(|s| split_comment_line(s, 80, 0))
+        .fold(String::new(), |x, y| (x + &y))
+}
