ba45d7969f86a252125faa56a9d186bed5e1a2ce
SoVictor <sovictor_tomsk@hotmail.com>
2020-03-17 03:53:36 +0700

    Minor changes in error messages and comments

--- xsd-types/src/types/utils.rs
+++ xsd-types/src/types/utils.rs
@@ -1,5 +1,6 @@
 use chrono::FixedOffset;
 
+// Parses ISO 8601 timezone.
 pub fn parse_timezone(s: &str) -> Result<FixedOffset, String> {
     if s == "Z" {
         return Ok(FixedOffset::east(0))
@@ -14,13 +15,13 @@ pub fn parse_timezone(s: &str) -> Result<FixedOffset, String> {
     let minutes = tokens[1].parse::<i32>().unwrap();
 
     if hours > 14 || (hours == 14 && minutes != 0) || minutes >= 60 {
-        return Err("bad timezone format".to_string())
+        return Err("bad timezone format: out of range".to_string())
     }
 
     let offset_secs = 60 * (60 * hours + minutes);
     match s.chars().next().unwrap() {
         '+' => Ok(FixedOffset::east(offset_secs)),
         '-' => Ok(FixedOffset::west(offset_secs)),
-        _ => Err("bad timezone format".to_string())
+        _ => Err("bad timezone format: timezone should start with '+' or '-'".to_string())
     }
 }
