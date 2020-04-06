4ef8761392fb023f884b40d4d197efe57941a671
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-06 23:44:33 +0700

    issue-65: Add integration test

--- wsdl-parser/src/lib.rs
+++ wsdl-parser/src/lib.rs
@@ -2,3 +2,5 @@ extern crate clap;
 
 pub mod generator;
 pub mod parser;
+#[cfg(test)]
+mod tests;
---
+++ wsdl-parser/src/tests/mod.rs
@@ -0,0 +1,46 @@
+use roxmltree::{Document, Node};
+use crate::parser::definitions::Definitions;
+use xsd_parser::generator::builder::GeneratorBuilder;
+use xsd_parser::parser::schema::parse_schema;
+use crate::generator::generate;
+
+mod port_type_to_function;
+
+pub fn generate_wsdl(input: &str) -> String {
+    let doc = Document::parse(input).unwrap();
+    let definitions = Definitions::new(&doc.root_element());
+    let gen = GeneratorBuilder::default().build();
+    let schemas = definitions
+        .types()
+        .iter()
+        .flat_map(|t| t.schemas())
+        .collect::<Vec<Node<'_, '_>>>();
+    let mut code = schemas
+        .iter()
+        .map(|f| gen.generate_rs_file(&parse_schema(f)))
+        .collect::<Vec<String>>();
+
+    code.push(generate(&definitions));
+    let code = code.join("");
+    code
+}
+
+pub fn assert_ast_eq(expected: &str, actual: &str) {
+    let expected = syn::parse_file(&expected).unwrap();
+    let actual = syn::parse_file(&actual).unwrap();
+
+    assert_eq!(expected, actual)
+}
+
+pub fn ast_test(input_wsdl: &str, expected_rs: &str) {
+    let expected = expected_rs;
+    let actual = generate_wsdl(input_wsdl);
+
+    println!("=== expected:\n{}", expected);
+    println!("=== actual:\n{}", actual);
+    println!("=== diff:\n");
+
+    text_diff::print_diff(expected, &actual, "\n");
+
+    assert_ast_eq(expected, &actual)
+}
