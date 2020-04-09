bf1f0dad9af5029aa60ad21a2be87c0daee31511
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-09 18:53:47 +0700

    xsd-model: add include stub

---
+++ xsd-parser/src/xsd_model/include.rs
@@ -0,0 +1,19 @@
+// See http://www.w3.org/TR/xmlschema-1/#element-include.
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: xmlschema.xsd
+// Type: Anonymous
+// Properties: Global, Qualified
+//
+// Content
+//  xsd:annotation [0..1]    from type xsd:annotated
+
+// Attributes
+// Any attribute	[0..*]		        Namespace: ##other, Process Contents: lax	from type xsd:openAttrs
+// id	            [0..1]	xsd:ID		                                            from type xsd:annotated
+// schemaLocation	[1..1]	xsd:anyURI
+
+// Used in Anonymous type of element xsd:schema
+
+pub struct Include {
+
+}
