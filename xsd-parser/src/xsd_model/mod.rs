9d07a9d61a653e2b334d5377744f79d30b074f9e
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-12 02:33:04 +0700

    xsd-model: wip

--- xsd-parser/src/xsd_model/mod.rs
+++ xsd-parser/src/xsd_model/mod.rs
@@ -1,20 +1,4 @@
-pub mod common_groups;
-pub mod element;
 pub mod facets;
-pub mod import;
-pub mod include;
-pub mod list;
-pub mod notation;
-pub mod redefine;
-pub mod schema;
-pub mod sequence;
-pub mod simple_content;
-pub mod simple_type;
-pub mod union;
-pub mod unique;
-pub mod key;
-pub mod key_ref;
-pub mod selector;
 
 pub mod complex_types;
 pub mod elements;
