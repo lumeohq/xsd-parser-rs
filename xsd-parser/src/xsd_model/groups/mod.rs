f2abac018a8ff23c10a8e99b5b5fd07ca554f383
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 23:45:05 +0700

    xsd-model: Add simpleRestrictionModel group

--- xsd-parser/src/xsd_model/groups/mod.rs
+++ xsd-parser/src/xsd_model/groups/mod.rs
@@ -2,3 +2,4 @@ pub mod attr_decls;
 pub mod complex_type_model;
 pub mod nested_particle;
 pub mod type_def_particle;
+pub mod simple_restriction_model;
---
+++ xsd-parser/src/xsd_model/groups/simple_restriction_model.rs
@@ -0,0 +1,32 @@
+use crate::xsd_model::complex_types::local_simple_type::LocalSimpleType;
+
+// xsd:simpleRestrictionModel
+// Group information
+// Namespace: http://www.w3.org/2001/XMLSchema
+//
+// Schema document: datatypes.xsd
+//
+// Content
+//  Sequence [1..1]
+//      xsd:simpleType [0..1]
+//      Choice [0..*]       from group xsd:facets
+//          xsd:minExclusive
+//          xsd:minInclusive
+//          xsd:maxExclusive
+//          xsd:maxInclusive
+//          xsd:totalDigits
+//          xsd:fractionDigits
+//          xsd:length
+//          xsd:minLength
+//          xsd:maxLength
+//          xsd:enumeration
+//          xsd:whiteSpace
+//          xsd:pattern
+//
+// Used in
+// Anonymous type of element xsd:restriction
+// Type xsd:simpleRestrictionType (Element xsd:restriction)
+pub struct SimpleRestrictionModel<'a> {
+    simple_type: Option<LocalSimpleType<'a>>,
+    facets: Option<FacetGroup<'a>>
+}
