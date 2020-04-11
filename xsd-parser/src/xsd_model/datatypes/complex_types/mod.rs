0119dd7cf4288a66b11f11da0284ec348540ef42
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 18:44:45 +0700

    xsd-model: Add top level simple type

--- xsd-parser/src/xsd_model/datatypes/complex_types/mod.rs
+++ xsd-parser/src/xsd_model/datatypes/complex_types/mod.rs
@@ -3,3 +3,4 @@ pub mod local_simple_type;
 pub mod num_facet;
 pub mod no_fixed_facet;
 pub mod simple_type;
+pub mod top_level_simple_type;
---
+++ xsd-parser/src/xsd_model/datatypes/complex_types/top_level_simple_type.rs
@@ -0,0 +1,37 @@
+
+// xsd:topLevelSimpleType
+// Complex type information
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: datatypes.xsd
+//
+// Content
+//  Sequence [1..1]
+//      xsd:annotation [0..1]
+//      Choice [1..1]           from group xsd:simpleDerivation
+//          xsd:restriction
+//          xsd:list
+//          xsd:union
+//
+// Attributes
+// id	            [0..1]	xsd:ID		                                                    from type xsd:annotated
+// final	        [0..1]	xsd:simpleDerivationSet		                                    from type xsd:simpleType
+// name	            [1..1]	xsd:NCName	            Required at the top level
+// Any attribute	[0..*]		                    Namespace: ##other, Process Contents: lax
+//
+// Used by
+// Element xsd:simpleType
+//
+// Type inheritance chain
+//  xsd:anyType
+//      xsd:openAttrs
+//          xsd:annotated
+//              xsd:simpleType
+//                  xsd:topLevelSimpleType
+pub struct TopLevelSimpleType<'a> {
+    annotation: Option<Annotation<'a>>,
+    content_choice: SimpleDerivation<'a>,
+    id: Option<Id<'a>>,
+    final_: Option<SimpleDerivationSet>,
+    name: NCName<'a>,
+    attributes: Vec<RawAttribute<'a>>
+}
