31b7954fbf1a163d6ae9024af21f1adaddd6ed1e
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 23:55:58 +0700

    xsd-model: Add Extension element

---
+++ xsd-parser/src/xsd_model/elements/extension.rs
@@ -0,0 +1,28 @@
+use crate::xsd_model::complex_types::simple_extension_type::SimpleExtensionType;
+use crate::xsd_model::complex_types::extension_type::ExtensionType;
+
+// Element information
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: xmlschema.xsd
+// Type: xsd:simpleExtensionType
+// Properties: Local, Qualified
+//
+// Used in
+// Anonymous type of element xsd:simpleContent
+pub type SimpleExtension<'a>  = SimpleExtensionType<'a>;
+
+
+// Element information
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: xmlschema.xsd
+// Type: xsd:extensionType
+// Properties: Local, Qualified
+//
+// Attributes
+// Any attribute	[0..*]		Namespace: ##other, Process Contents: lax	from type xsd:openAttrs
+// id	[0..1]	xsd:ID		from type xsd:annotated
+// base	[1..1]	xsd:QName
+//
+// Used in
+// Anonymous type of element xsd:complexContent
+pub type Extension<'a> = ExtensionType<'a>;
