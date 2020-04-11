e2ddb4dad17f06137ca016141747e07fa3537b95
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 21:31:03 +0700

    xsd-model: Add appinfo

---
+++ xsd-parser/src/xsd_model/elements/app_info.rs
@@ -0,0 +1,27 @@
+use crate::xsd_model::simple_types::any_uri::AnyUri;
+use crate::xsd_model::{RawElement, RawAttribute};
+
+// xsd:appinfo
+// See http://www.w3.org/TR/xmlschema-1/#element-appinfo.
+// Element information
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: xmlschema.xsd
+// Type: Anonymous
+// Properties: Global, Qualified
+//
+// Content
+// Any text (mixed) content, intermingled with:
+// Any element [0..*] Namespace: ##any, Process Contents: lax
+//
+// Attributes
+// source	        [0..1]	xsd:anyURI
+// Any attribute	[0..*]		Namespace: ##other, Process Contents: lax
+//
+// Used in
+// Anonymous type of element xsd:annotation
+pub struct AppInfo<'a> {
+    pub text: &'a str,
+    pub elements: Vec<RawElement<'a>>,
+    pub source: Option<AnyUri<'a>>,
+    pub attributes: Vec<RawAttribute<'a>>
+}
--- xsd-parser/src/xsd_model/elements/mod.rs
+++ xsd-parser/src/xsd_model/elements/mod.rs
@@ -3,3 +3,4 @@ pub mod fraction_digits;
 pub mod length;
 pub mod list;
 pub mod annotation;
+pub mod app_info;
