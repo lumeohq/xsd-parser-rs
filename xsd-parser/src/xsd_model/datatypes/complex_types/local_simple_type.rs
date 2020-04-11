55e6b151f95ad90e79ebcc7eca3a257c4eff6da2
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 18:33:21 +0700

    xsd-model: Add local simple type

---
+++ xsd-parser/src/xsd_model/datatypes/complex_types/local_simple_type.rs
@@ -0,0 +1,33 @@
+
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: datatypes.xsd
+// Type: xsd:localSimpleType
+// Properties: Local, Qualified
+//
+// Content
+//  Sequence [1..1]
+//      xsd:annotation [0..1]
+//      Choice [1..1]    from group xsd:simpleDerivation
+//          xsd:restriction
+//          xsd:list
+//          xsd:union
+//
+// Attributes
+// id	            [0..1]	xsd:ID		                                        from type xsd:annotated
+// Any attribute	[0..*]		    Namespace: ##other, Process Contents: lax
+//
+// Used by
+// Element xsd:simpleType
+//
+// Type inheritance chain
+//  xsd:anyType
+//      xsd:openAttrs
+//          xsd:annotated
+//              xsd:simpleType
+//                  xsd:localSimpleType
+pub struct LocalSimpleType<'a> {
+    annotation: Option<Annotation<'a>>,
+    content_choice: Box<SimpleDerivation<'a>>,
+    id: Option<Id<'a>>,
+    attributes: Vec<RawAttribute<'a>>
+}
--- xsd-parser/src/xsd_model/datatypes/complex_types/mod.rs
+++ xsd-parser/src/xsd_model/datatypes/complex_types/mod.rs
@@ -1,1 +1,2 @@
 pub mod facet;
+pub mod local_simple_type;
