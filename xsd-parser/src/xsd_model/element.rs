ce86f9eb761229477f3155cea50fdf5d454ec134
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 16:39:35 +0700

    xsd-model: Add TopLevelElement element stub

---
+++ xsd-parser/src/xsd_model/element.rs
@@ -0,0 +1,39 @@
+
+
+// See http://www.w3.org/TR/xmlschema-1/#element-element.
+// Element information
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: xmlschema.xsd
+// Type: xsd:topLevelElement
+// Properties: Global, Qualified
+//
+// Content
+//  Sequence [1..1]
+//      xsd:annotation [0..1]               from type xsd:annotated
+//      Choice [0..1]                       from group xsd:elementModel
+//          xsd:simpleType
+//          xsd:complexType
+//      Choice [0..*]           from group xsd:identityConstraint
+//          xsd:unique
+//          xsd:key
+//          xsd:keyref
+//
+// Attributes
+// Any attribute	    [0..*]		                Namespace: ##other, Process Contents: lax	from type xsd:openAttrs
+// id	                [0..1]	xsd:ID		                                                    from type xsd:annotated
+// name	                [1..1]	xsd:NCName
+// type	                [0..1]	xsd:QName
+// substitutionGroup	[0..1]	xsd:QName
+// default	            [0..1]	xsd:string
+// fixed	            [0..1]	xsd:string
+// nillable	            [0..1]	xsd:boolean		    Default value is "false".
+// abstract	            [0..1]	xsd:boolean		    Default value is "false".
+// final	            [0..1]	xsd:derivationSet
+// block	            [0..1]	xsd:blockSet
+//
+// Used in
+// Group xsd:schemaTop
+// Anonymous type of element xsd:schema via reference to xsd:schemaTop
+pub struct TopLevelElement<'a> {
+
+}
