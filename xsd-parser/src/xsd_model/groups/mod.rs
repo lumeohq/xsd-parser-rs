fa98d4af3c8c06fdcbc224fe23a28d024b0314a1
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 23:34:51 +0700

    xsd-model: Add typeDefParticle group

--- xsd-parser/src/xsd_model/groups/mod.rs
+++ xsd-parser/src/xsd_model/groups/mod.rs
@@ -1,3 +1,4 @@
 pub mod attr_decls;
 pub mod complex_type_model;
 pub mod nested_particle;
+pub mod type_def_particle;
---
+++ xsd-parser/src/xsd_model/groups/type_def_particle.rs
@@ -0,0 +1,26 @@
+use crate::xsd_model::elements::all::AllType;
+use crate::xsd_model::elements::choice::Choice;
+
+// xsd:typeDefParticle
+// 'complexType' uses this
+// Group information
+// Namespace: http://www.w3.org/2001/XMLSchema
+//
+// Schema document: xmlschema.xsd
+//
+// Content
+//  Choice [1..1]
+//      xsd:group
+//      xsd:all    An "all" group that allows elements to appear in any order. Unlike other group types, does not allow other groups as children, only elements.
+//      xsd:choice
+//      xsd:sequence
+//
+// Used in
+// Group xsd:complexTypeModel
+// Type xsd:complexRestrictionType (Element xsd:restriction)
+// Type xsd:extensionType (Element xsd:extension)
+pub enum TypeDefParticle<'a> {
+    Group(Group<'a>),
+    All(AllType<'a>),
+    Choice(Choice<'a>),
+    Sequence(Sequence<'a>)
