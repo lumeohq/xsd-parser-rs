74475d11115f6c70330295e463fda9f2ea8fcf7f
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 18:36:42 +0700

    xsd-model: Add no fixed facet

---
+++ xsd-parser/src/xsd_model/datatypes/complex_types/no_fixed_facet.rs
@@ -0,0 +1,28 @@
+
+// xsd:noFixedFacet
+// Complex type information
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: datatypes.xsd
+//
+// Content
+// xsd:annotation [0..1]
+//
+// Attributes
+// id	            [0..1]	xsd:ID		from type xsd:annotated
+// value	        [1..1]	xsd:anySimpleType		from type xsd:facet
+// Any attribute	[0..*]		Namespace: ##other, Process Contents: lax
+//
+// Used by
+// Element xsd:enumeration
+// Element xsd:pattern via derived anonymous type
+//
+// Type inheritance chain
+//  xsd:anyType
+//      xsd:openAttrs
+//          xsd:annotated
+//              xsd:facet
+//                  xsd:noFixedFacet
+//                      restricted by Anonymous type of element xsd:pattern
+pub struct NoFixedFacet {
+    
+}
