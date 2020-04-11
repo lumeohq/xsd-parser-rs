dc999436a79fd419347bca6ba3e8f47d00891451
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-11 20:59:12 +0700

    xsd-model: Add list element

---
+++ xsd-parser/src/xsd_model/datatypes/elements/list.rs
@@ -0,0 +1,24 @@
+use crate::xsd_model::datatypes::simple_types::Id;
+
+// Element information
+// Namespace: http://www.w3.org/2001/XMLSchema
+// Schema document: datatypes.xsd
+// Type: Anonymous
+// Properties: Global, Qualified
+//
+// Content
+//  Sequence [1..1]
+//      xsd:annotation [0..1]    from type xsd:annotated
+//      xsd:simpleType [0..1]
+//
+// Attributes
+// Any attribute	[0..*]		        Namespace: ##other, Process Contents: lax	from type xsd:openAttrs
+// id	            [0..1]	xsd:ID		                                            from type xsd:annotated
+// itemType	        [0..1]	xsd:QName
+pub struct List<'a> {
+    annotation: Option<Annotation<'a>>,
+    simple_type: Option<Box<LocalSimpleType<'a>>>,
+    attributes: Vec<RawAttribute<'a>>,
+    id: Id<'a>,
+    item_type: Option<QName<'a>>
+}
--- xsd-parser/src/xsd_model/datatypes/elements/mod.rs
+++ xsd-parser/src/xsd_model/datatypes/elements/mod.rs
@@ -1,3 +1,4 @@
 pub mod enumeration;
 pub mod fraction_digits;
 pub mod length;
+pub mod list;
