def5ef75be0bd29688ae5c250e75c3171b479d7a
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-17 19:10:47 +0700

    xsd-model: add SimpleDerivationSet parsing

--- xsd-parser/src/xsd_model/simple_types/simple_derivation_set.rs
+++ xsd-parser/src/xsd_model/simple_types/simple_derivation_set.rs
@@ -21,13 +21,62 @@
 //
 // Used by
 // Attribute final
+#[derive(Debug, PartialEq)]
 pub enum SimpleDerivationSet {
     All,
     List(Vec<SimpleDerivationSubset>),
 }
 
+impl SimpleDerivationSet {
+    pub fn parse(s: &str) -> Result<Self, String> {
+        let res = match s {
+            "#all" => Self::All,
+            _ => {
+                let res: Result<Vec<_>, String> = s
+                    .split(' ')
+                    .map(|s| SimpleDerivationSubset::parse(s))
+                    .collect();
+                Self::List(res?)
+            }
+        };
+        Ok(res)
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum SimpleDerivationSubset {
     List,
     Union,
     Restriction,
+}
+
+impl SimpleDerivationSubset {
+    pub fn parse(s: &str) -> Result<Self, String> {
+        let res = match s {
+            "list" => Self::List,
+            "union" => Self::Union,
+            "restriction" => Self::Restriction,
+            _ => return Err(format!("Invalid value for SimpleDerivationSubset: {}", s)),
+        };
+        Ok(res)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::SimpleDerivationSet;
+    use super::SimpleDerivationSubset::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(
+            SimpleDerivationSet::parse("#all").unwrap(),
+            SimpleDerivationSet::All
+        );
+        assert_eq!(
+            SimpleDerivationSet::parse("list union restriction").unwrap(),
+            SimpleDerivationSet::List(vec![List, Union, Restriction])
+        );
+        assert!(SimpleDerivationSet::parse("val").is_err());
+    }
 }
