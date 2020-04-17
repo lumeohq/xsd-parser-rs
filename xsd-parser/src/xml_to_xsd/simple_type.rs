4c445dcd3a43c0878488bb4d12b0c3cb47851a3b
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-17 19:11:00 +0700

    xsd-model: add TopLevelSimpleType parsing

--- xsd-parser/src/xml_to_xsd/simple_type.rs
+++ xsd-parser/src/xml_to_xsd/simple_type.rs
@@ -1,8 +1,10 @@
 use crate::xml_to_xsd::XsdNode;
 use crate::xsd_model::elements::ElementType;
 use crate::xsd_model::groups::simple_derivation::SimpleDerivation;
+use crate::xsd_model::simple_types::ncname::NCName;
+use crate::xsd_model::simple_types::simple_derivation_set::SimpleDerivationSet;
 use crate::xsd_model::{Annotation, Restriction};
-use crate::xsd_model::{List, LocalSimpleType, Union};
+use crate::xsd_model::{List, LocalSimpleType, TopLevelSimpleType, Union};
 use roxmltree::Node;
 
 impl<'a> LocalSimpleType<'a> {
@@ -35,6 +37,56 @@ impl<'a> LocalSimpleType<'a> {
     }
 }
 
+impl<'a> TopLevelSimpleType<'a> {
+    pub fn parse(node: Node<'a, '_>) -> Result<Self, String> {
+        let mut annotation = None;
+        let mut content_choice = None;
+        let mut id = None;
+        let mut final_: Option<SimpleDerivationSet> = None;
+        let mut name: Option<NCName> = None;
+        let mut attributes = vec![];
+
+        for ch in node.children().filter(|n| n.is_element()) {
+            match ch.xsd_type()? {
+                ElementType::Annotation => annotation = Some(Annotation::parse(ch)?),
+                x => content_choice = Some(SimpleDerivation::parse(ch, x)?),
+            };
+        }
+
+        let content_choice = content_choice.ok_or_else(|| {
+            format!(
+                "Content required for xsd:TopLevelSimpleType type: {:?}",
+                node
+            )
+        })?;
+
+        for attr in node.attributes() {
+            match attr.name() {
+                "id" => id = Some(attr.into()),
+                "final" => final_ = Some(SimpleDerivationSet::parse(attr.value())?),
+                "name" => name = Some(attr.into()),
+                _ => attributes.push(attr.clone()),
+            }
+        }
+
+        let name = name.ok_or_else(|| {
+            format!(
+                "Content required for xsd:TopLevelSimpleType type: {:?}",
+                node
+            )
+        })?;
+
+        Ok(Self {
+            annotation,
+            content_choice,
+            id,
+            final_,
+            name,
+            attributes,
+        })
+    }
+}
+
 impl<'a> SimpleDerivation<'a> {
     pub fn parse(
         node: Node<'a, '_>,
@@ -48,5 +100,32 @@ impl<'a> SimpleDerivation<'a> {
         };
 
         Ok(res)
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::xsd_model::groups::simple_derivation::SimpleDerivation;
+    use crate::xsd_model::TopLevelSimpleType;
+
+    #[test]
+    fn test_top_level_simple_type_parse() {
+        let doc = roxmltree::Document::parse(
+            r##"<simpleType id="ID" name="Type1" final="#all" a='b' b='a'>
+                        <list itemType="itemType" />
+                    </simpleType>"##,
+        )
+        .unwrap();
+        let root = doc.root_element();
+        let res = TopLevelSimpleType::parse(root).unwrap();
+        assert!(res.annotation.is_none());
+        assert_eq!(res.attributes.len(), 2);
+        assert_eq!(res.id.unwrap().0, "ID");
+        assert_eq!(res.name.0, "Type1");
+        match &res.content_choice {
+            SimpleDerivation::List(x) => assert_eq!(x.item_type.as_ref().unwrap().name, "itemType"),
+            _ => unreachable!("test failed!"),
+        }
     }
 }
