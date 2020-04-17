368fa6209c4b747d516703d17cc26727290b3c4d
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-17 14:10:19 +0700

    xsd-model: add test for explicit_group

--- xsd-parser/src/xml_to_xsd/explicit_group.rs
+++ xsd-parser/src/xml_to_xsd/explicit_group.rs
@@ -2,7 +2,6 @@ use roxmltree::Node;
 
 use crate::xml_to_xsd::XsdNode;
 use crate::xsd_model::elements::ElementType;
-use std::str::ParseBoolError;
 
 use crate::xsd_model::complex_types::explicit_group::ExplicitGroup;
 use crate::xsd_model::groups::nested_particle::NestedParticle;
@@ -29,5 +28,38 @@ impl<'a> ExplicitGroup<'a> {
         }
 
         Ok(res)
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::xsd_model::complex_types::explicit_group::ExplicitGroup;
+    use crate::xsd_model::MaxOccurs::Bounded;
+
+    #[test]
+    fn test_parse() {
+        let doc = roxmltree::Document::parse(
+            r###"
+            <xs:sequence xmlns:xs="http://www.w3.org/2001/XMLSchema" a='a' b='a' maxOccurs='5'>
+                <xs:element name="Parameters" type="xs:string" />
+                <xs:element name="Parameters2" type="xs:string" />
+                <xs:any namespace="##any" processContents="lax" minOccurs="0" maxOccurs="unbounded"/>
+            </xs:sequence>
+            "###,
+        )
+        .unwrap();
+        let root = doc.root_element();
+        let res = ExplicitGroup::parse(root).unwrap();
+        assert!(res.annotation.is_none());
+        let np = &res.nested_particle;
+        assert_eq!(np.len(), 3);
+
+        assert_eq!(res.attributes.len(), 2);
+        assert_eq!(res.min_occurs.0, 1);
+        match &res.max_occurs {
+            Bounded(x) => assert_eq!(x.0, 5),
+            _ => unreachable!()
+        }
     }
 }
--- xsd-parser/src/xml_to_xsd/nested_particle.rs
+++ xsd-parser/src/xml_to_xsd/nested_particle.rs
@@ -2,15 +2,16 @@ use crate::xsd_model::groups::nested_particle::NestedParticle;
 use roxmltree::Node;
 use crate::xml_to_xsd::XsdNode;
 use crate::xsd_model::elements::ElementType;
+use crate::xsd_model::complex_types::explicit_group::ExplicitGroup;
 
 impl<'a> NestedParticle<'a> {
     pub fn parse(node: Node<'a, '_>) -> Result<Self, String> {
         let res = match node.xsd_type()? {
-            // ElementType::Element => Self::Element(Facet::parse(node)?),
-            // ElementType::Group => Self::Group(Facet::parse(node)?),
-            // ElementType::Choice => Self::Choice(Facet::parse(node)?),
-            // ElementType::Sequence => Self::Sequence(Facet::parse(node)?),
-            // ElementType::Any => Self::Any(TotalDigits::parse(node)?),
+            // ElementType::Element => Self::Element(::parse(node)?),
+            // ElementType::Group => Self::Group(::parse(node)?),
+            ElementType::Choice => Self::Choice(ExplicitGroup::parse(node)?),
+            ElementType::Sequence => Self::Sequence(ExplicitGroup::parse(node)?),
+            // ElementType::Any => Self::Any(::parse(node)?),
             _ => return Err(format!("Error NestedParticle parsing. Invalid node: {:?}", node)),
         };
 
