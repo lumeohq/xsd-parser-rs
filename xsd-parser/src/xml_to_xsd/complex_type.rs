752ef319adae983e8c949fe13c07c0508693c956
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-04-18 14:42:47 +0700

    xsd-model: wip

--- xsd-parser/src/xml_to_xsd/complex_type.rs
+++ xsd-parser/src/xml_to_xsd/complex_type.rs
@@ -1,8 +1,8 @@
-use roxmltree::Node;
-use crate::xsd_model::TopLevelComplexType;
+// use roxmltree::Node;
+// use crate::xsd_model::TopLevelComplexType;
 
-impl<'a> TopLevelComplexType<'a> {
-    pub fn parse(node: Node<'a, '_>) -> Result<Self, String> {
-
-    }
-}
+// impl<'a> TopLevelComplexType<'a> {
+//     pub fn parse(node: Node<'a, '_>) -> Result<Self, String> {
+//
+//     }
+// }
--- xsd-parser/src/xml_to_xsd/type_def_particle.rs
+++ xsd-parser/src/xml_to_xsd/type_def_particle.rs
@@ -1,17 +1,22 @@
-use crate::xsd_model::groups::type_def_particle::TypeDefParticle;
-use crate::xsd_model::elements::ElementType;
-use roxmltree::Node;
-use crate::xsd_model::complex_types::named_group::NamedGroup;
 use crate::xsd_model::complex_types::explicit_group::ExplicitGroup;
+use crate::xsd_model::complex_types::named_group::NamedGroup;
+use crate::xsd_model::elements::ElementType;
+use crate::xsd_model::groups::type_def_particle::TypeDefParticle;
+use roxmltree::Node;
 
 impl<'a> TypeDefParticle<'a> {
-    pub fn parse(node: Node, element_type: ElementType) -> Result<Self, String> {
+    pub fn parse(node: Node<'a, '_>, element_type: ElementType) -> Result<Self, String> {
         Ok(match element_type {
             ElementType::Group => TypeDefParticle::Group(NamedGroup::parse(node)?),
             ElementType::All => unimplemented!("Not presented in ONVIF"), //TypeDefParticle::All(),
             ElementType::Choice => TypeDefParticle::Choice(ExplicitGroup::parse(node)?),
             ElementType::Sequence => TypeDefParticle::Sequence(ExplicitGroup::parse(node)?),
-            _ => return Err(format!("Invalid node for xsd:typeDefParticle group: {:?}", node))
+            _ => {
+                return Err(format!(
+                    "Invalid node for xsd:typeDefParticle group: {:?}",
+                    node
+                ))
+            }
         })
     }
 }
