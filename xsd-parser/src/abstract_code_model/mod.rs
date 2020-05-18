5f3ac4fb93c3abe7c0524c5c20ccf9f3f01b8ec0
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-05-18 22:42:58 +0700

    xsd-model: add struct

--- xsd-parser/src/abstract_code_model/mod.rs
+++ xsd-parser/src/abstract_code_model/mod.rs
@@ -1,7 +1,17 @@
 pub mod comment;
 pub mod module;
+pub mod r#struct;
+pub mod alias;
+
+use crate::abstract_code_model::comment::Comment;
+use crate::abstract_code_model::module::Module;
+use crate::abstract_code_model::r#struct::Struct;
 
 
-pub enum Entity {
+#[derive(Debug)]
+pub enum Entity<'a> {
+    Module(Module<'a>),
+    Comment(Comment<'a>),
+    Struct(Struct<'a>),
 
 }
