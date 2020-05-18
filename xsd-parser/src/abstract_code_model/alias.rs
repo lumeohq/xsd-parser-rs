cde545545d2c7f271e18bec04ca733e70c6bcaf7
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-05-18 23:00:23 +0700

    xsd-model: add enum

--- xsd-parser/src/abstract_code_model/alias.rs
+++ xsd-parser/src/abstract_code_model/alias.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 use crate::abstract_code_model::comment::Comment;
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Default)]
 pub struct Alias<'a> {
     pub name: Cow<'a, str>,
     pub type_name: Cow<'a, str>,
--- xsd-parser/src/abstract_code_model/mod.rs
+++ xsd-parser/src/abstract_code_model/mod.rs
@@ -1,11 +1,14 @@
 pub mod comment;
 pub mod module;
 pub mod r#struct;
+pub mod r#enum;
 pub mod alias;
 
 use crate::abstract_code_model::comment::Comment;
 use crate::abstract_code_model::module::Module;
 use crate::abstract_code_model::r#struct::Struct;
+use crate::abstract_code_model::r#enum::Enum;
+use crate::abstract_code_model::alias::Alias;
 
 
 #[derive(Debug)]
@@ -13,5 +16,6 @@ pub enum Entity<'a> {
     Module(Module<'a>),
     Comment(Comment<'a>),
     Struct(Struct<'a>),
-
+    Alias(Alias<'a>),
+    Enum(Enum<'a>)
 }
