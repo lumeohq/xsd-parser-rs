c54faff9decdfb57020c6397b27c3e5e5ddc749c
Nithin Philips <nithin@nithinphilips.com>
2020-06-10 10:51:51 -0400

    Upgrade to yaserde 0.4x and implement serialize_attributes for YaSerialize

--- macro-utils/src/lib.rs
+++ macro-utils/src/lib.rs
@@ -35,6 +35,10 @@ pub fn default_serde(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
             fn serialize<W: Write>(&self, writer: &mut yaserde::ser::Serializer<W>) -> Result<(), String> {
                 utils::yaserde::serialize(self, #struct_name_literal, writer, |s| s.to_string())
             }
+
+            fn serialize_attributes(&self, attributes: Vec<xml::attribute::OwnedAttribute>, namespace: xml::namespace::Namespace) -> Result<(Vec<xml::attribute::OwnedAttribute>, xml::namespace::Namespace), String> {
+                Ok((attributes, namespace))
+            }
         }
 
         impl YaDeserialize for #struct_name {
