7790d58b6a95d4be423dda38372fe18d1055f1c1
FabriceDS <fabrice.simonin@gmail.com>
2020-11-14 15:46:31 +0100

    Add support for xsd:all to xsd-parser

--- xsd-parser/src/parser/all.rs
+++ xsd-parser/src/parser/all.rs
@@ -5,13 +5,13 @@ use roxmltree::Node;
 use crate::parser::types::{RsEntity, Struct};
 use crate::parser::utils::{elements_to_fields, get_documentation, get_parent_name};
 
-pub fn parse_sequence(sequence: &Node, parent: &Node) -> RsEntity {
-    let name = get_parent_name(sequence);
+pub fn parse_all(node: &Node, parent: &Node) -> RsEntity {
+    let name = get_parent_name(node);
     RsEntity::Struct(Struct {
         name: name.into(),
         comment: get_documentation(parent),
         subtypes: vec![],
-        fields: RefCell::new(elements_to_fields(sequence, name)),
+        fields: RefCell::new(elements_to_fields(node, name)),
         ..Default::default()
     })
 }
--- xsd-parser/src/parser/utils.rs
+++ xsd-parser/src/parser/utils.rs
@@ -6,7 +6,7 @@ use roxmltree::{Namespace, Node};
 
 use crate::parser::constants::attribute;
 use crate::parser::node_parser::parse_node;
-use crate::parser::types::{Enum, RsEntity, StructField, StructFieldSource, Alias};
+use crate::parser::types::{Alias, Enum, RsEntity, StructField, StructFieldSource, TypeModifier};
 use crate::parser::xsd_elements::{ElementType, XsdNode};
 
 pub fn target_namespace<'a, 'input>(node: &Node<'a, 'input>) -> Option<&'a Namespace<'input>> {
@@ -62,9 +62,7 @@ pub fn attributes_to_fields(node: &Node) -> Vec<StructField> {
 
 pub fn attribute_groups_to_aliases(node: &Node) -> Vec<Alias> {
     node.children()
-        .filter(|n| {
-            n.xsd_type() == ElementType::AttributeGroup
-        })
+        .filter(|n| n.xsd_type() == ElementType::AttributeGroup)
         .map(|n| match parse_node(&n, node) {
             RsEntity::Alias(a) => a,
             _ => unreachable!("Invalid attribute group parsing: {:?}", n),
@@ -80,4 +78,23 @@ pub fn enum_to_field(en: Enum) -> StructField {
         source: StructFieldSource::Element,
         ..Default::default()
     }
+}
+
+pub fn elements_to_fields(node: &Node, parent_name: &str) -> Vec<StructField> {
+    node.children()
+        .filter(|n| n.is_element() && n.xsd_type() != ElementType::Annotation)
+        .map(|n| match parse_node(&n, node) {
+            RsEntity::StructField(mut sf) => {
+                if sf.type_name.ends_with(parent_name) {
+                    sf.type_modifiers.push(TypeModifier::Recursive)
+                }
+                sf
+            }
+            RsEntity::Enum(mut en) => {
+                en.name = format!("{}Choice", parent_name);
+                enum_to_field(en)
+            }
+            _ => unreachable!("\nError: {:?}\n{:?}", n, parse_node(&n, node)),
+        })
+        .collect()
 }
