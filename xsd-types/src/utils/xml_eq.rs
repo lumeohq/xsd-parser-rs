8a69ee808c24bb268359f13bd05bb0114d39cd6c
Jonas Platte <jonas@lumeo.com>
2021-04-19 19:09:39 +0200

    Remove dependency on itertools

--- xsd-types/src/utils/xml_eq.rs
+++ xsd-types/src/utils/xml_eq.rs
@@ -1,15 +1,13 @@
-use itertools::izip;
+use xml::reader::{Error as XmlError, XmlEvent};
 
 pub fn assert_xml_eq(actual: &str, expected: &str) {
-    for (a, e) in izip!(without_whitespaces(actual), without_whitespaces(expected)) {
+    for (a, e) in without_whitespaces(actual).zip(without_whitespaces(expected)) {
         assert_eq!(a, e);
     }
 }
 
-fn without_whitespaces(
-    expected: &str,
-) -> impl Iterator<Item = Result<xml::reader::XmlEvent, xml::reader::Error>> + '_ {
+fn without_whitespaces(expected: &str) -> impl Iterator<Item = Result<XmlEvent, XmlError>> + '_ {
     xml::EventReader::new(expected.as_bytes())
         .into_iter()
-        .filter(|e| !matches!(e, Ok(xml::reader::XmlEvent::Whitespace(_))))
+        .filter(|e| !matches!(e, Ok(XmlEvent::Whitespace(_))))
 }
