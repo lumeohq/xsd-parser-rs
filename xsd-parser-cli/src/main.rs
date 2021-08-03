30b808dc10ca70f81516176e2d322b8c7ef3135e
Dr. Chat <arkolbed@gmail.com>
2021-08-02 21:54:52 -0500

    Use anyhow for error reporting

--- xsd-parser-cli/src/main.rs
+++ xsd-parser-cli/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 
+use anyhow::Context;
+
 use std::fs;
 use std::io::{prelude::*, Read};
 use std::path::{Path, PathBuf};
@@ -8,7 +10,7 @@ use std::fs::OpenOptions;
 use xsd_parser::generator::builder::GeneratorBuilder;
 use xsd_parser::parser::parse;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let matches = App::new("xsd-parser-rs")
         .about("An xsd/wsdl => rust code generator written in rust")
         .arg(
@@ -33,20 +35,20 @@ fn main() {
     let md = fs::metadata(input_path).unwrap();
     if md.is_dir() {
         let output_path = Path::new(output_path.unwrap_or("output/rs"));
-        process_dir(input_path, output_path)
+        process_dir(input_path, output_path)?;
     } else {
-        process_single_file(input_path, output_path)
+        process_single_file(input_path, output_path)?;
     }
-    .map_err(|e| println!("Error: {}", e))
-    .unwrap();
+
+    Ok(())
 }
 
-fn process_dir(input_path: &Path, output_path: &Path) -> Result<(), String> {
+fn process_dir(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
     if !output_path.exists() {
-        fs::create_dir_all(output_path).map_err(|e| e.to_string())?;
+        fs::create_dir_all(output_path)?;
     }
-    for entry in fs::read_dir(input_path).map_err(|e| e.to_string())? {
-        let path = entry.map_err(|e| e.to_string())?.path();
+    for entry in fs::read_dir(input_path)? {
+        let path = entry?.path();
         if path.is_dir() {
             process_dir(&path, &output_path.join(path.file_name().unwrap()))?;
         } else {
@@ -58,32 +60,31 @@ fn process_dir(input_path: &Path, output_path: &Path) -> Result<(), String> {
     Ok(())
 }
 
-fn process_single_file(input_path: &Path, output_path: Option<&str>) -> Result<(), String> {
+fn process_single_file(input_path: &Path, output_path: Option<&str>) -> anyhow::Result<()> {
     let text = load_file(input_path)?;
-    let rs_file = parse(text.as_str()).map_err(|_| "Error parsing file".to_string())?;
+    let rs_file = parse(text.as_str()).map_err(|_| anyhow::anyhow!("Error parsing file"))?;
     let gen = GeneratorBuilder::default().build();
     let code = gen.generate_rs_file(&rs_file);
     if let Some(output_filename) = output_path {
-        write_to_file(output_filename, &code).map_err(|e| format!("Error writing file: {}", e))?;
+        write_to_file(output_filename, &code).context("Error writing file")?;
     } else {
         println!("{}", code);
     }
     Ok(())
 }
 
-fn load_file(path: &Path) -> Result<String, String> {
-    let mut file = fs::File::open(&path).map_err(|e| e.to_string())?;
+fn load_file(path: &Path) -> std::io::Result<String> {
+    let mut file = fs::File::open(&path)?;
     let mut text = String::new();
-    file.read_to_string(&mut text).map_err(|e| e.to_string())?;
+    file.read_to_string(&mut text)?;
     Ok(text)
 }
 
-fn write_to_file(path: &str, text: &str) -> Result<(), String> {
+fn write_to_file(path: &str, text: &str) -> std::io::Result<()> {
     let mut file = OpenOptions::new()
         .write(true)
         .truncate(true)
         .create(true)
-        .open(path)
-        .map_err(|e| e.to_string())?;
-    file.write_all(text.as_bytes()).map_err(|e| e.to_string())
+        .open(path)?;
+    file.write_all(text.as_bytes())
 }
