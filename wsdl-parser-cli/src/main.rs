7096535bba0460ff3af9e077a614676c85ea786b
Justin Moore <DrChat@users.noreply.github.com>
2021-08-03 09:33:54 -0500

    Use anyhow for error reporting

--- wsdl-parser-cli/src/main.rs
+++ wsdl-parser-cli/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 
+use anyhow::Context;
+
 use roxmltree::{Document, Node};
 use std::fs;
 use std::io::{prelude::*, Read};
@@ -9,7 +11,7 @@ use wsdl_parser::parser::definitions::Definitions;
 use xsd_parser::generator::builder::GeneratorBuilder;
 use xsd_parser::parser::schema::parse_schema;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let matches = App::new("wsdl-parser")
         .about("An wsdl => rust code generator written in rust")
         .arg(
@@ -35,21 +37,21 @@ fn main() {
     let md = fs::metadata(input_path).unwrap();
     if md.is_dir() {
         let output_path = Path::new(output_path.unwrap_or("output/wsdl-rs"));
-        process_dir(input_path, output_path)
+        process_dir(input_path, output_path)?;
     } else {
-        process_single_file(input_path, output_path)
+        process_single_file(input_path, output_path)?;
     }
-    .map_err(|e| println!("Error: {}", e))
-    .unwrap();
+
+    Ok(())
 }
 
 //TODO: Add a common mechanism for working with files
-fn process_dir(input_path: &Path, output_path: &Path) -> Result<(), String> {
+fn process_dir(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
     if !output_path.exists() {
-        fs::create_dir_all(output_path).map_err(|e| e.to_string())?;
+        fs::create_dir_all(output_path)?;
     }
-    for entry in fs::read_dir(input_path).map_err(|e| e.to_string())? {
-        let path = entry.map_err(|e| e.to_string())?.path();
+    for entry in fs::read_dir(input_path)? {
+        let path = entry?.path();
         if path.is_dir() {
             process_dir(&path, &output_path.join(path.file_name().unwrap()))?;
         } else {
@@ -61,9 +63,9 @@ fn process_dir(input_path: &Path, output_path: &Path) -> Result<(), String> {
     Ok(())
 }
 
-fn process_single_file(input_path: &Path, output_path: Option<&str>) -> Result<(), String> {
+fn process_single_file(input_path: &Path, output_path: Option<&str>) -> anyhow::Result<()> {
     let text = load_file(input_path)?;
-    let doc = Document::parse(text.as_str()).unwrap();
+    let doc = Document::parse(text.as_str()).context("Failed to parse input document")?;
     let definitions = Definitions::new(&doc.root_element());
     let gen = GeneratorBuilder::default().build();
     let schemas = definitions
@@ -79,21 +81,23 @@ fn process_single_file(input_path: &Path, output_path: Option<&str>) -> Result<(
     code.push(generate(&definitions));
     let code = code.join("");
     if let Some(output_filename) = output_path {
-        write_to_file(output_filename, &code).map_err(|e| format!("Error writing file: {}", e))?;
+        write_to_file(output_filename, &code).context("Error writing file")?;
     } else {
         println!("{}", code);
     }
     Ok(())
 }
 
-fn load_file(path: &Path) -> Result<String, String> {
-    let mut file = fs::File::open(&path).map_err(|e| e.to_string())?;
+fn load_file(path: &Path) -> std::io::Result<String> {
+    let mut file = fs::File::open(&path)?;
     let mut text = String::new();
-    file.read_to_string(&mut text).map_err(|e| e.to_string())?;
+    file.read_to_string(&mut text)?;
     Ok(text)
 }
 
-fn write_to_file(path: &str, text: &str) -> Result<(), String> {
-    let mut file = fs::File::create(path).map_err(|e| e.to_string())?;
-    file.write_all(text.as_bytes()).map_err(|e| e.to_string())
+fn write_to_file(path: &str, text: &str) -> std::io::Result<()> {
+    let mut file = fs::File::create(path)?;
+    file.write_all(text.as_bytes())?;
+
+    Ok(())
 }
