07da98fd29911c3a01e703bc0643de87eeeb3187
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2024-03-19 15:28:53 +0700

    Upgrade `roxmltree`

--- xsd-parser/src/parser/schema.rs
+++ xsd-parser/src/parser/schema.rs
@@ -8,15 +8,18 @@ use crate::parser::{
 };
 
 pub fn parse_schema<'input>(schema: &Node<'_, 'input>) -> RsFile<'input> {
+    let mut xsd_namespaces = schema
+        .namespaces()
+        .filter(|namespace| namespace.uri() == "http://www.w3.org/2001/XMLSchema");
+
     RsFile {
         name: "".into(),
         namespace: None,
         target_ns: target_namespace(schema).cloned(),
-        xsd_ns: schema
-            .namespaces()
-            .iter()
-            .rev()
-            .find(|a| a.uri() == "http://www.w3.org/2001/XMLSchema")
+        xsd_ns: xsd_namespaces
+            .clone()
+            .find(|namespace| namespace.name().is_some())
+            .or_else(|| xsd_namespaces.next())
             .cloned(),
         types: schema
             .children()
@@ -40,6 +43,24 @@ mod test {
     use crate::parser::schema::parse_schema;
 
     #[test]
+    fn test_single_xsd_ns() {
+        let doc = roxmltree::Document::parse(
+            r#"
+    <xs:schema
+        xmlns:tt="http://www.onvif.org/ver10/schema"
+        xmlns:xs="http://www.w3.org/2001/XMLSchema"
+        targetNamespace="http://www.onvif.org/ver10/schema"
+        >
+    </xs:schema>
+                "#,
+        )
+        .unwrap();
+
+        let res = parse_schema(&doc.root_element());
+        assert_eq!(res.xsd_ns.unwrap().name().unwrap(), "xs");
+    }
+
+    #[test]
     fn test_multiple_xsd_ns() {
         let doc = roxmltree::Document::parse(
             r#"
@@ -56,6 +77,6 @@ mod test {
         .unwrap();
 
         let res = parse_schema(&doc.root_element());
-        assert_eq!(res.xsd_ns.unwrap().name().unwrap(), "xsd");
+        assert_eq!(res.xsd_ns.unwrap().name().unwrap(), "xs");
     }
 }
