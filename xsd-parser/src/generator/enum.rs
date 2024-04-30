574e6c080af6cd4bf79c494ced79b83628ee87f8
Brent Gardner <bgardner@squarelabs.net>
2024-04-30 17:17:53 -0600

    hacky hack

--- xsd-parser/src/generator/enum.rs
+++ xsd-parser/src/generator/enum.rs
@@ -38,8 +38,11 @@ pub trait EnumGenerator {
             validation = self.validation(entity, gen),
         );
 
-        let str2 = format!("impl YaSerialize for Box<{name}> {{\n\tfn serialize<W: Write>(&self, writer: &mut Serializer<W>) -> Result<(), String> {{\nself.serialize(writer)\n}}\nfn serialize_attributes(&self, attributes: Vec<OwnedAttribute>, namespace: Namespace) -> Result<(Vec<OwnedAttribute>, Namespace), String> {{\nself.serialize_attributes(attributes, namespace)\n}}\n}}\n\n");
-        let str3 = format!("impl YaDeserialize for Box<{name}> {{\n\tfn deserialize<R: Read>(reader: &mut Deserializer<R>) -> Result<Self, String> {{\n\t\tOk(Box::new({name}::deserialize(reader)?))}}\n}}\n\n");
+        let mut str2 = format!("impl YaSerialize for Box<{name}> {{\n");
+        str2 += format!("\tfn serialize<W: Write>(&self, writer: &mut Serializer<W>) -> Result<(), String> {{\n\t\tself.as_ref().serialize(writer)\n}}\n").as_str();
+        str2 += format!("\tfn serialize_attributes(&self, attributes: Vec<OwnedAttribute>, namespace: Namespace) -> Result<(Vec<OwnedAttribute>, Namespace), String> {{\n\t\tself.as_ref().serialize_attributes(attributes, namespace)\n}}\n}}\n\n").as_str();
+        let mut str3 = format!("impl YaDeserialize for Box<{name}> {{\n\tfn deserialize<R: Read>(reader: &mut Deserializer<R>) -> Result<Self, String> {{\n");
+        str3 += format!("\t\tOk(Box::new({name}::deserialize(reader)?))\n}}\n}}\n\n").as_str();
         str + str2.as_str() + str3.as_str()
     }
 
