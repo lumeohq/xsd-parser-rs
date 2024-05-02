c5099f9e865cdbb916465a62f16f6eb2013f1c06
Brent Gardner <bgardner@squarelabs.net>
2024-05-02 12:33:30 -0600

    hacky hack

--- xsd-parser/src/generator/struct.rs
+++ xsd-parser/src/generator/struct.rs
@@ -86,7 +86,7 @@ pub trait StructGenerator {
     }
 
     fn macros(&self, _entity: &Struct, gen: &Generator) -> Cow<'static, str> {
-        let derives = "#[derive(Clone, Default, PartialEq, Debug, YaSerialize, YaDeserialize)]\n";
+        let derives = "#[derive(Default, PartialEq, Debug, YaSerialize, YaDeserialize)]\n";
         let tns = gen.target_ns.borrow();
         match tns.as_ref() {
             Some(tn) => match tn.name() {
